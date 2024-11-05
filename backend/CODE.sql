d918a995394e0e00b1b9ce2c1ad809fa7b4340a7
CHANDRANIBHA <mayturajesh@gmail.com>
2024-11-05 13:17:47 +0530

    code

--- backend/CODE.sql
+++ backend/CODE.sql
@@ -10,3 +10,21 @@ CREATE TABLE `students_tbl` (
   PRIMARY KEY (`std_id`),
   UNIQUE KEY `std_id` (`std_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE asap.class_tbl (
+    class_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    class_name VARCHAR(50) NOT NULL,
+    semester INT NOT NULL, -- Stores semester number
+    additional_info TEXT -- Optional, for any other information
+);
+
+
+
+CREATE TABLE `teacher_tbl` (
+  `teacher_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(25) NOT NULL,
+  `subject_id` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`teacher_id`),
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
